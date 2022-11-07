c537cccd41d5053d099f8e0f7df02ff9d7b9d393
Leonardo Suarez Restrepo <leonardosuarez16@hotmail.com>
2022-11-07 13:35:43 -0500

    backend listo con la funcion de token funcionado

--- src/strategies/admin.strategy.ts
+++ src/strategies/admin.strategy.ts
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken");
 
 
 export class EstrategiaAdministrador implements AuthenticationStrategy{
+   // en este espacio podemos definir mas estrategias para definir para un vendedor o asesor etc en vex de admin
+  //ponemos vendedor o otro
   name: string = 'admin';
 
   constructor(
@@ -21,12 +23,15 @@ export class EstrategiaAdministrador implements AuthenticationStrategy{
     if (token) {
       let datos = this.servicioAutenticacion.ValidarTokenJWT(token);
       if (datos) {
-        let perfil: UserProfile = Object.assign({
-          nombre: datos.data.nombres
-        });
-        return perfil;
-        ///if (datos.data.roles) en caso que queramos poner los rol del usuario
-
+        // en esta linea pondriamos los roles depues de data.rol para que supueramos quien lo tiene con la funcion de generaltokenjwt
+        if (datos) {
+          // en este lugar tendriamos que validar el rol que tendriamos asignados en los datos
+          let perfil: UserProfile = Object.assign({
+            nombre: datos.data.nombres
+          });
+          return perfil;
+          ///if (datos.data.roles) en caso que queramos poner los rol del usuario
+        }
       } else {
         throw new HttpErrors [401]("el token incluido no es valido")
       }
